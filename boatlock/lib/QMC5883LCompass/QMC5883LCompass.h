044728ba11cde3c18f4df9c12846bccbd0e60688
dslimp <54681111+dslimp@users.noreply.github.com>
2025-06-18 08:47:29 +0300

    Add QMC5883L compass support

--- boatlock/lib/QMC5883LCompass/QMC5883LCompass.h
+++ boatlock/lib/QMC5883LCompass/QMC5883LCompass.h
@@ -1,42 +1,74 @@
-#pragma once
-#include <Wire.h>
-#include <Arduino.h>
+#ifndef QMC5883L_Compass
+#define QMC5883L_Compass
 
-class QMC5883LCompass {
-public:
-    bool begin(uint8_t addr = 0x0D) {
-        _addr = addr;
-        Wire.begin();
-        if (!writeReg(0x0B, 0x01)) return false; // Set/reset period
-        // Continuous mode, ODR=200Hz, RNG=8G, OSR=512
-        if (!writeReg(0x09, 0x1D)) return false;
-        return true;
-    }
+#include "Arduino.h"
+#include "Wire.h"
 
-    void update() {
-        Wire.beginTransmission(_addr);
-        Wire.write(0x00);
-        if (Wire.endTransmission() != 0) return;
-        Wire.requestFrom((int)_addr, 6);
-        if (Wire.available() < 6) return;
-        int16_t x = Wire.read() | (Wire.read() << 8);
-        int16_t y = Wire.read() | (Wire.read() << 8);
-        int16_t z = Wire.read() | (Wire.read() << 8);
-        (void)z;
-        heading = atan2((float)y, (float)x) * 180.0f / PI;
-        if (heading < 0) heading += 360.0f;
-    }
 
-    float getHeading() const { return heading; }
+class QMC5883LCompass{
+	
+  public:
+    QMC5883LCompass();
+	void init();
+    void setADDR(byte b);
+    void setMode(byte mode, byte odr, byte rng, byte osr);
+	void setMagneticDeclination(int degrees, uint8_t minutes);
+	void setSmoothing(byte steps, bool adv);
+	void calibrate();
+	void setCalibration(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max);
+	void setCalibrationOffsets(float x_offset, float y_offset, float z_offset);
+	void setCalibrationScales(float x_scale, float y_scale, float z_scale);
+    float getCalibrationOffset(uint8_t index);
+	float getCalibrationScale(uint8_t index);
+	void clearCalibration();
+	void setReset();
+    void read();
+	int getX();
+	int getY();
+	int getZ();
+	int getAzimuth();
+	byte getBearing(int azimuth);
+	void getDirection(char* myArray, int azimuth);
 
-private:
-    uint8_t _addr = 0x0D;
-    float heading = 0.0f;
-
-    bool writeReg(uint8_t reg, uint8_t val) {
-        Wire.beginTransmission(_addr);
-        Wire.write(reg);
-        Wire.write(val);
-        return Wire.endTransmission() == 0;
-    }
+  private:
+    void _writeReg(byte reg,byte val);
+	int _get(int index);
+	float _magneticDeclinationDegrees = 0;
+	bool _smoothUse = false;
+	byte _smoothSteps = 5;
+	bool _smoothAdvanced = false;
+    byte _ADDR = 0x0D;
+	int _vRaw[3] = {0,0,0};
+	int _vHistory[10][3];
+	int _vScan = 0;
+	long _vTotals[3] = {0,0,0};
+	int _vSmooth[3] = {0,0,0};
+	void _smoothing();
+	float _offset[3] = {0.,0.,0.};
+	float _scale[3] = {1.,1.,1.};
+	int _vCalibrated[3];
+	void _applyCalibration();
+	const char _bearings[16][3] =  {
+		{' ', ' ', 'N'},
+		{'N', 'N', 'E'},
+		{' ', 'N', 'E'},
+		{'E', 'N', 'E'},
+		{' ', ' ', 'E'},
+		{'E', 'S', 'E'},
+		{' ', 'S', 'E'},
+		{'S', 'S', 'E'},
+		{' ', ' ', 'S'},
+		{'S', 'S', 'W'},
+		{' ', 'S', 'W'},
+		{'W', 'S', 'W'},
+		{' ', ' ', 'W'},
+		{'W', 'N', 'W'},
+		{' ', 'N', 'W'},
+		{'N', 'N', 'W'},
+	};
+	
+	
+	
 };
+
+#endif
